88cd8dd0894d5222919067875f38a021d2afee1a
shotttik <shota.akhlouri@gmail.com>
2022-11-14 22:24:49 +0400

    created database models

--- sql_app/database.py
+++ sql_app/database.py
@@ -7,4 +7,4 @@ SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost/tictactoe"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-base = declarative_base()
+Base = declarative_base()
---
+++ sql_app/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Boolean, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import ARRAY
+from .database import Base
+
+class Game(Base):
+    __tablename__ = "games"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    board = Column(ARRAY(Integer))
+    finished = Column(Boolean, default=True)
+    
+    histories = relationship("histories", back_populates="game")
+
+class History(Base):
+    __tablename__ = "histories"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    type = Column(String)
+    position = Column(Integer)
+    result = Column(Boolean, default=True)
+    game_id = Column(Integer, ForeignKey("games.id"))
+    
+    game = relationship("Game", back_populates="histories")
+    
